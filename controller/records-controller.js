4eaa5be839d8fa0a103360c48013d886c97aa3ec
Nathalie Scherf <nascherf@yahoo.se>
2021-02-09 14:54:29 +0100

    Records controller mit async await

--- controller/records-controller.js
+++ controller/records-controller.js
@@ -1,24 +1,29 @@
-// Vereinheitlicht Datenbank
-const db = require('../db');
 // importiere das Model für records: 
 const Record = require('../models/recordmodel')
+const createError = require('http-errors')
 
-exports.recordsGetAllController = (req, res, next) => {
-	//res.send('ich zeige alle Produkte des Ladens als Array');
-	const aufnahmen = db
-		.get('records')
-		.value()
-	res.status(200).send(aufnahmen);
+
+exports.recordsGetAllController = async (req, res, next) => {
+	try {
+		let aufnahmen = await Record.find()
+		res.status(200).send(aufnahmen)
+	} catch (error) {
+		next(error)
+	}
 }
 
-exports.recordsGetOneController = (req, res, next) => {
-	const { id } = req.params;
-	const record = db
-		.get('records')
-		.find({ id });
-	res
-		.status(200)
-		.send(record);
+exports.recordsGetOneController = async (req, res, next) => {
+	try {
+		const { id } = req.params;
+		let aufnahme = await Record.find({ _id: id })
+		// kommt keine eine aufnahme zurück?
+		if (aufnahme.length < 1) throw new Error()
+		res.status(200).send(aufnahme)
+	} catch (error) {
+		console.log(error);
+		let fehler = createError(404, `Die Aufnahme mit dem ID ${req.params.id} gibt es nicht`)
+		next(fehler)
+	}
 }
 
 // arbeitet schon mit Mongoose:
@@ -31,24 +36,30 @@ exports.recordsPostController = async (req, res, next) => {
 	}
 }
 
-exports.recordsPutController = (req, res, next) => {
-	const { id } = req.params;
-
-	const geänderteWerte = req.body;
-	const Aufnahme = db
-		.get('records')
-		.find({ id })
-		.assign(geänderteWerte)
-		.write();
-	res.status(200).send(Aufnahme);
+exports.recordsPutController = async (req, res, next) => {
+	try {
+		const { id } = req.params;
+		const geänderteWerte = req.body;
+		let antwort = await Record.updateOne({ _id: id }, { ...geänderteWerte })
+		res.status(200).send(antwort)
+	} catch (error) {
+		console.log(error);
+		let fehler = createError(404, `Konnte die Aufnahme mit dem ID ${req.params.id}  nicht updaten`)
+		next(fehler)
+	}
 }
 
-exports.recordsDeleteController = (req, res, next) => {
-
-	const { id } = req.params;
-	const record = db
-		.get('records')
-		.remove({ id })
-		.write();
-	res.status(200).send(record);
+exports.recordsDeleteController = async (req, res, next) => {
+	try {
+		const { id } = req.params;
+		let antwort = await Record.deleteOne({ _id: id })
+		if (antwort.deletedCount > 0) {
+			res.status(200).send('erfolgreich gelöscht')
+		} else {
+			res.send('Es gab keinen Eintrag zum Löschen!')
+		}
+	} catch (error) {
+		let fehler = createError(404, `Konnte die Aufnahme mit dem ID ${req.params.id}  nicht löschen`)
+		next(fehler)
+	}
 }
